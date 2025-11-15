1acb3a4ed2456a5e363cd33dd5d1f9bd8e3139fa
Sonia Lerner <sonialerner05@gmail.com>
2025-11-15 18:57:35 -0500

    added junit

--- Client.java
+++ Client.java
@@ -1,3 +1,37 @@
+import java.util.*;
+import java.net.*;
+import java.time.*;
+import java.io.*;
+
 public class Client {
+
+    String host ;
+    int port ;
+    Socket clientSocket ;
+
+    Client(String host, int port) {
+        this.host = host ;
+        this.port = port ;
+    }
     
+    public void handshake() {
+
+    }
+
+    public void disconnect() {
+
+    }
+
+    public void close() {
+
+    }
+
+    public String request(String string) {
+
+        return string ;
+    }
+
+    public Socket getSocket() {
+        return this.clientSocket ;
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,29 @@
+import java.util.*;
+import java.net.*;
+import java.time.*;
+import java.io.*;
+
 public class Server {
-    
+    private int port ;
+    private static final int passkey = 12345 ;
+
+    Server(int port) {
+        this.port = port ;
+    }
+
+    public void serve(int a) {
+
+        for(int i = 0; i < a; i++) {
+            // process the client request in a separate thread
+        }
+
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes() {
+        return new ArrayList<>() ;
+    }
+
+    public void disconnect() {
+
+    }
 }
