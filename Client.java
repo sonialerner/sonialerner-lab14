fff95221194000aeb6806801c08b408bc4a6a551
Sonia Lerner <sonialerner05@gmail.com>
2025-11-16 23:54:08 -0500

    all methods roughly drafted

--- Client.java
+++ Client.java
@@ -1,34 +1,42 @@
-import java.util.*;
+// import java.util.*;
 import java.net.*;
-import java.time.*;
+// import java.time.*;
 import java.io.*;
 
 public class Client {
 
-    String host ;
-    int port ;
-    Socket clientSocket ;
+    String host;
+    int port;
+    Socket clientSocket = null ;
+    BufferedReader reader = null;
+    PrintWriter writer = null;
 
-    Client(String host, int port) {
-        this.host = host ;
-        this.port = port ;
+    Client(String host, int port) throws IOException {
+        this.clientSocket = new Socket(host, port) ;
+        this.reader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+        this.writer = new PrintWriter(this.clientSocket.getOutputStream(), true);
     }
     
     public void handshake() {
-
+        this.writer.write("12345");
+        this.writer.println();
     }
 
-    public void disconnect() {
-
+    public void disconnect() throws IOException {
+        this.reader.close(); 
+        this.writer.close();
+        this.clientSocket.close();
     }
 
-    public void close() {
+    public String request(String string) throws IOException {
+        String message = string;
+        this.writer.write(message);
+        this.writer.println();
+        String line = "";
 
-    }
+        line = this.reader.readLine();
 
-    public String request(String string) {
-
-        return string ;
+        return line ;
     }
 
     public Socket getSocket() {
--- Server.java
+++ Server.java
@@ -4,26 +4,43 @@ import java.time.*;
 import java.io.*;
 
 public class Server {
-    private int port ;
-    private static final int passkey = 12345 ;
-
-    Server(int port) {
-        this.port = port ;
+    String host;
+    int port;
+    ServerSocket serverSocket = null ;
+    Socket threadSocket = null ;
+    BufferedReader reader = null;
+    PrintWriter writer = null;
+    ArrayList<LocalDateTime> connectedTimes = null;
+    String passkey ;
+    
+    Server(int i) throws IOException{
+        this.port = i ;
+        this.reader = new BufferedReader(new InputStreamReader(System.in)) ;
+        this.writer = new PrintWriter(this.threadSocket.getOutputStream(), true);
     }
-
-    public void serve(int a) {
-
+    
+    public void serve(int a) throws IOException {
         for(int i = 0; i < a; i++) {
             // process the client request in a separate thread
+            // passkey check
+            if(this.reader.readLine().equals(this.passkey)) { this.threadSocket = serverSocket.accept() ; }
+            
+            this.threadSocket.close() ;
         }
-
     }
-
+    
     public ArrayList<LocalDateTime> getConnectedTimes() {
         return new ArrayList<>() ;
     }
-
+    
     public void disconnect() {
-
+        try {
+            this.reader.close(); 
+            this.writer.close();
+            this.threadSocket.close();
+            this.serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
